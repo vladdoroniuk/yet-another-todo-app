f00b9000b8fd93448aeaa623d902eb1ed4fd3dfd
vladdoroniuk <vladyslav.doroniuk@gmail.com>
2023-01-27 20:18:57 +0200

    Add and configure tRPC

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Yet another TODO app</h1>;
-}
+import { trpc } from "../utils/trpc";
+
+const Home = () => {
+  const res = trpc.todos.useQuery();
+
+  if (!res.data) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="flex h-screen">
+      <div className="m-auto">
+        <h1 className="text-3xl font-bold">Yet Another Todo App</h1>
+        <ul>
+          {res.data.todos.map((item) => (
+            <li key={item.id}>{item.todo}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/server/routers/_app.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+import { procedure, router } from "../trpc";
+
+const db_data = [
+  {
+    id: 1,
+    todo: "Read something",
+  },
+  {
+    id: 2,
+    todo: "Write something",
+  },
+];
+
+export const appRouter = router({
+  todos: procedure.query(() => {
+    return {
+      todos: db_data,
+    };
+  }),
+});
+// export type definition of API
+export type AppRouter = typeof appRouter;
